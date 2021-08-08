193ae6df55a1ea252cd8a4812c351ba6e2f9b32d
purusottam 2011 <ppurusottam1@gmail.com>
2021-08-08 23:49:54 +0530

    first commit

---
+++ src/test/java/com/purusottam/instagram/InstagramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.purusottam.instagram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
